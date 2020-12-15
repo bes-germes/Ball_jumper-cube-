de56b4dc7b177d1c4bbf7e3a9e09819f2a854b9f
bes-germes <55259601+bes-germes@users.noreply.github.com>
2020-12-15 18:02:09 +0300

    Update ball.cpp

--- ball.cpp
+++ ball.cpp
@@ -8,30 +8,52 @@ Ball::Ball(QWidget *parent) : QWidget(parent) {
     setStyleSheet("background-color:white;");
     inGame = true;
 
+
     resize(B_WIDTH, B_HEIGHT);
     loadImages();
     initGame();
 }
 
 void Ball::loadImages() {
-
+    spike.load("C:\\Users\\Oleg\\Pictures\\Saved Pictures\\spike.png");
     head.load("C:\\Users\\Oleg\\Pictures\\Saved Pictures\\head.png");
     field.load("C:\\Users\\Oleg\\Pictures\\Saved Pictures\\field.png");
     bad_pixel.load("C:\\Users\\Oleg\\Pictures\\Saved Pictures\\dot.png");
+    block.load("C:\\Users\\Oleg\\Pictures\\Saved Pictures\\apple.png");
+    white_screen.load("C:\\Users\\Oleg\\Pictures\\Saved Pictures\\white_screen.png");
+    field_1.load("C:\\Users\\Oleg\\Pictures\\Saved Pictures\\field_1.png");
 }
 
 
 void Ball::initGame() {
+    gameOverBool = 0;
+    NumberOfLevel = 0;
 
-    x = 800;
+    dots = 2;
+    x = 1920;
     y = 500;
+
+
     groundPixel = 500;
-    maxRight = 1400;
+    //levelOfHight[0] = 500;
+    prev_groundPixel= 500;
+    maxRight = 1920;
+    maxLeft = 0;
+    booljump = false;
+    boolDontReapeatJump = false;
+
     timerId = startTimer(DELAY);
 }
 
-void Ball::locateBadPixel(){
+bool Ball::checkBoolArray(){
+    for (int i = 0; i < 114; ++i){
+        if (test[i] != true){
+            return false;
+        }else{
 
+        }
+    }
+    return true;
 }
 
 void Ball::paintEvent(QPaintEvent *e) {
@@ -45,16 +67,129 @@ void Ball::doDrawing() {
 
     QPainter qp(this);
 
+    switch (NumberOfLevel){
+        case 0:
+            qp.drawImage(0, 0, field);
+
+            for (int i = 0; i < 71; ++i) {
+                block_x[i] = 0 + i * 20;
+                block_y[i] = 510;
+                //levelOfHight[1] = 490;
+                qp.drawImage(block_x[i], block_y[i], block);
+            }
+            for (int i = 71; i < 74; ++i){
+                block_x[i] = 1400 + (i - 71) * 20;
+                block_y[i] = 490;
+                //levelOfHight[1] = 490;
+                qp.drawImage(block_x[i], block_y[i], block);
+            }
+
+            for (int i = 74; i < 77; ++i){
+                block_x[i] = 1440 + (i - 74) * 20;
+                block_y[i] = 470;
+                //levelOfHight[2] = 470;
+                qp.drawImage(block_x[i], block_y[i], block);
+            }
+            for (int i = 77; i < 80; ++i){
+                block_x[i] = 1460 + (i - 77) * 20;
+                block_y[i] = 450;
+                //levelOfHight[2] = 470;
+                qp.drawImage(block_x[i], block_y[i], block);
+            }
+            for (int i = 80; i < 92; ++i){
+                block_x[i] = 1500 + (i - 80) * 20;
+                block_y[i] = 430;
+                //levelOfHight[2] = 470;
+                qp.drawImage(block_x[i], block_y[i], block);
+            }
+            for (int i = 92; i < 103; ++i){
+                block_x[i] = 1700 + (i - 92) * 20;
+                block_y[i] = 410;
+                //levelOfHight[2] = 470;
+                qp.drawImage(block_x[i], block_y[i], block);
+            }
+            for (int i = 103; i < 104; ++i){
+                block_x[i] = 1900 + (i - 103) * 20;
+                block_y[i] = 390;
+                //levelOfHight[2] = 470;
+                qp.drawImage(block_x[i], block_y[i], block);
+            }
+            for (int i = 104; i < 107; ++i){
+                block_x[i] = 120 + (i - 104) * 20;
+                block_y[i] = 490;
+                //levelOfHight[2] = 470;
+                qp.drawImage(block_x[i], block_y[i], block);
+            }
+            for (int i = 107; i < 109; ++i){
+                block_x[i] = 140 + (i - 107) * 20;
+                block_y[i] = 470;
+                //levelOfHight[2] = 470;
+                qp.drawImage(block_x[i], block_y[i], block);
+            }
+            for (int i = 109; i < 112; ++i){
+                block_x[i] = 220 + (i - 109) * 20;
+                block_y[i] = 490;
+                //levelOfHight[2] = 470;
+                qp.drawImage(block_x[i], block_y[i], block);
+            }
+            for (int i = 112; i < 114; ++i){
+                block_x[i] = 220 + (i - 112) * 20;
+                block_y[i] = 470;
+                //levelOfHight[2] = 470;
+                qp.drawImage(block_x[i], block_y[i], block);
+            }
+            for (int i = 0; i < 2; ++i){
+                spike_x[i] = 180 + i * 20;
+                spike_y[i] = 500;
+                qp.drawImage(spike_x[i], spike_y[i], spike);
+            }
+
+        break;
+
+        case 1:
+            qp.drawImage(0, 0, field_1);
+
+
+            for (int i = 0; i < 114; ++i) {
+                block_x[i] = 0 + i * 20;
+                block_y[i] = 390;
+                //levelOfHight[1] = 490;
+                qp.drawImage(block_x[i], block_y[i], block);
+            }
+
+        break;
+    }
+
     if (inGame) {
 
-        qp.drawImage(0, 0, field);
         qp.drawImage(x, y, head);
 
-
     } else {
-
+        qp.drawImage(0, 0, white_screen);
+        if (NumberOfLevel == 1){
+            finishGame(&qp, "You have won","(press escape to restart)");
+        }else{
+            finishGame(&qp, "Game lost","(press escape to restart)");
+        }
 
     }
+
+}
+
+void Ball::finishGame(QPainter *painter, QString message,QString message2) {
+
+  QFont font("Courier", 15, QFont::DemiBold);
+  QFontMetrics fm(font);
+  int textWidth = fm.horizontalAdvance(message);
+  int textWidth2 = fm.horizontalAdvance(message2);
+
+  painter->setFont(font);
+  int h = height();
+  int w = width();
+
+  painter->translate(QPoint(w/2, h/2));
+  painter->drawText(-textWidth/2, -10, message);
+  painter->drawText(-textWidth2/2, 10, message2);
 }
 
 void Ball::keyPressEvent(QKeyEvent *e) {
@@ -75,12 +210,17 @@ void Ball::keyPressEvent(QKeyEvent *e) {
 
     }
 
-    if (key == Qt::Key_Up && booljump != true && boolDontReapeatJump != true) {
+    if ((key == Qt::Key_Up || key == Qt::Key_Space) && booljump != true && boolDontReapeatJump != true) {
 
         booljump = true;
         boolDontReapeatJump = true;
     }
 
+    if (key == Qt::Key_Escape){
+        x = 0;
+        y = 500;
+        inGame = true;
+    }
     QWidget::keyPressEvent(e);
 }
 
@@ -100,7 +240,7 @@ void Ball::keyReleaseEvent(QKeyEvent *e) {
 
 void Ball::jump(){
 
-    if (booljump && y > groundPixel - 40){
+    if (booljump && y > groundPixel - 50){
 
         y -= 10;
 
@@ -123,76 +263,112 @@ void Ball::down(){
 void Ball::move(){
 
     if (rightDirection && x <= maxRight){
-        x += DOT_SIZE;
+        x += 5;
     }
 
-    if (leftDirection){
-        x -= DOT_SIZE;
+    if (leftDirection && x >= maxLeft){
+        x -= 5;
     }
 }
 
-void Ball::moveObject(){
-
-
+void Ball::updateLevel(){
+    NumberOfLevel = 1;
+    x = 0;
+    y = 350;
 }
 
 void Ball::checkCollision(){
-    if(x <= 1400){
-        groundPixel = 500;
+
+    for (int i = 0; i < 114; ++i) {
+
+
+            if(x >= block_x[i] - 15 && x <= block_x[i] + 25){
+
+                if(x <= block_x[i] ){
+
+                maxRight = block_x[i] - 15;
+
+                if (block_y[i] - 10 >= y ){
+
+                    groundPixel = block_y[i] - 10;
+                    prev_groundPixel = groundPixel - 30;
+                    maxRight = block_x[i];
+                    }
+                }
+
+                if(x >= block_x[i]){
+
+                maxLeft = block_x[i] + 25;
+
+                if (block_y[i] - 10 >= y){
+                    groundPixel = block_y[i] - 10;
+                    prev_groundPixel = groundPixel - 30;
+                    maxLeft = block_x[i];
+                    }
+                }
+            }
+
+            if (x < block_x[i] - 15 || x > block_x[i] + 25){
+                test[i] = true;
+            }else{
+                test[i] = false;
+            }
+
+
+        if (checkBoolArray()){
+            groundPixel = prev_groundPixel;
+        }
+        if (maxLeft == block_x[0]){
+            maxLeft = 0;
+        }
+        if (maxRight == block_x[104]){
+            maxRight = 1905;
+        }
+        if (x >= block_x[102]){
+            updateLevel();
+        }
+
     }
-    if(x > 1400 && x <= 1430){
-        groundPixel = 480;
+    for (int i = 0; i < 2; ++i){
+        if (x >= spike_x[i]  && x <= spike_x[i] + 15 && y >= spike_y[i]){
+            groundPixel = spike_y[i] - 10;
+            prev_groundPixel = groundPixel - 30;
+            inGame = false;
+        }
     }
-    if(x > 1430 && x <= 1455){
-        groundPixel = 460;
-    }
-    if(x > 1455 && x <= 1490){
-        groundPixel = 440;
-    }
-    if(x > 1490 && x <= 1705){
-        groundPixel = 420;
-    }
-    if(x > 1705 && x <= 1880){
-        groundPixel = 400;
-    }
-    if (groundPixel == 500 && (y > 480 && y <= 505)){
-         maxRight = 1400;
-    }
-    if (groundPixel == 480 && (y > 460 && y <= 480)){
-        maxRight = 1430;
-    }
-    if (groundPixel == 460 && (y > 440 && y <= 460)){
-        maxRight = 1455;
-    }
-    if (groundPixel == 440 && (y > 420 && y <= 440)){
-        maxRight = 1490;
-    }
-    if (groundPixel == 420 && (y > 400 && y <= 420)){
-        maxRight = 1705;
-    }
-    if (groundPixel == 400 && (y > 380 && y <= 400)){
-        maxRight = 1880;
-    }
+
 }
 
 void Ball::terminalOut() {
 
   QTextStream out(stdout);
-  out << "Ground pixel = " << groundPixel << endl;
   out << "x = " << x << endl;
   out << "y = " << y << endl;
+  out << "numberOfLevel = " << NumberOfLevel << endl;
+  out << "gameOver = " << gameOverBool << endl;
+  out << "Right = " << maxRight << endl;
+  out << "prev_Ground pixel = " << prev_groundPixel << endl;
+
 }
 
-void Ball::timerEvent(QTimerEvent *e) {
 
+
+
+
+void Ball::timerEvent(QTimerEvent *e) {
+    //QPainter qp(this);
     Q_UNUSED(e);
 
     if (inGame) {
+
        checkCollision();
        move();
        jump();
        terminalOut();
 
+    }else{
+
     }
     repaint();
 }
+
