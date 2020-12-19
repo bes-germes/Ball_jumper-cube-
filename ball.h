8ff05a2176dc29bbfaaffbb5943d5b62adac4ccb
bes-germes <55259601+bes-germes@users.noreply.github.com>
2020-12-19 09:58:45 +0300

    Update ball.h

--- ball.h
+++ ball.h
@@ -1,12 +1,23 @@
 #pragma once
 
+#include <QPushButton>
 #include <QWidget>
 #include <QKeyEvent>
+#include <QPixmap>
+#include <QColor>
+#include <QThread>
+#include <QEventLoop>
+
 
 class Ball : public QWidget {
 
     public:
         Ball(QWidget *parent = 0);
+
+        static void msleep(int ms)
+                {
+                    QThread::msleep(ms);
+                }
 
     protected:
         void paintEvent(QPaintEvent *);
@@ -17,37 +28,80 @@ class Ball : public QWidget {
         QImage head;
         QImage field;
         QImage bad_pixel;
+        QImage block;
+        QImage spike;
+        QImage white_screen;
+        QImage field_1;
+        QImage field_2;
+        QImage block1;
+
+
+        QEventLoop loop;
+
+        QColor clrCurrent_Down;
+        QColor clrCurrent_Up;
+        QColor clrCurrent_Right;
+        QColor clrCurrent_Left;
+        QRgb qRgb;
 
         static const int B_WIDTH = 1920;
         static const int B_HEIGHT = 1080;
         static const int DOT_SIZE = 5;
-        static const int DELAY = 45;
+        static const int DELAY = 20;
 
         int timerId;
 
+        bool test[256];
+        bool test1[256];
+
         int x;
         int y;
+        int block_down_green;
+        int block_down_blue;
+        int block_left;
+        int block_right;
+        int spike_down;
+        int spike_left;
+        int spike_right;
 
+
+        int badBlock_x;
+        int badBlock_y;
         int groundPixel;
+        int prev_groundPixel;
+        int next_groundPixel;
+        int cur_groundPixel;
         int maxRight;
+        int maxLeft;
+        int NumberOfLevel;
+        int initNumber;
 
+        bool gameOverBool;
+        bool boolDown;
         bool boolDontReapeatJump;
         bool inGame;
         bool booljump;
         bool drawField;
         bool rightDirection;
         bool leftDirection;
+        bool testspike;
 
         void loadImages();
         void initGame();
         void locateBadPixel();
-        void checkApple();
         void moveObject();
         void checkCollision();
         void jump();
+        void updateLevel();
         void down();
         void move();
         void terminalOut();
         void doDrawing();
+        bool checkBoolArray();
+        void finishGame(QPainter *, QString,QString);
         void gameOver(QPainter &);
 };
+
+
+
+
